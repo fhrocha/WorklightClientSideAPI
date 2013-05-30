a41813fe5888ac581d683b605d47aed49cb7fa0f
Fabio Henrique da Rocha <fhrocha@gmail.com>
2013-05-30 13:34:23 -0300

    App

--- WorklightClientSideAPIProject/apps/WorklightClientSideAPI/common/js/WorklightClientSideAPI.js
+++ WorklightClientSideAPIProject/apps/WorklightClientSideAPI/common/js/WorklightClientSideAPI.js
@@ -5,28 +5,40 @@
 * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 */
 
+var busy;
+
 function wlCommonInit(){
 
-	/*
-	 * Application is started in offline mode as defined by a connectOnStartup property in initOptions.js file.
-	 * In order to begin communicating with Worklight Server you need to either:
-	 * 
-	 * 1. Change connectOnStartup property in initOptions.js to true. 
-	 *    This will make Worklight framework automatically attempt to connect to Worklight Server as a part of application start-up.
-	 *    Keep in mind - this may increase application start-up time.
-	 *    
-	 * 2. Use WL.Client.connect() API once connectivity to a Worklight Server is required. 
-	 *    This API needs to be called only once, before any other WL.Client methods that communicate with the Worklight Server.
-	 *    Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
-	
-	
-	// Common initialization code goes here
+	busy = new WL.BusyIndicator("AppBody", {text : 'Loading. Please wait...', bounceAnimation: true});
+	displayInfo();
+}
 
+function displayInfo() {
+	
+	var selectedValue = $('#actions').val();
+	var text = "";
+	
+	switch (selectedValue) {
+		case 'language':
+			text = WL.App.getDeviceLanguage();
+			break;
+		case 'appEnvironment':
+			text = WL.Client.getEnvironment();
+			break;
+		default:
+			break;
+	}
+	
+	$('#info').html(text);
+}
+
+function reload() {
+	
+	busy.show();
+	setTimeout(WL.Client.reloadApp, 5000);
+}
+
+function loadWebPage() {
+	
+	WL.App.openURL('http://m.ibm.com', '_blank');
 }
